31c55c640ce6683818d39b8e51a9b709c6f79a63
Kyle Knudson <kyle.knudson2015@gmail.com>
2018-10-26 17:39:26 -0500

    Add files via upload

--- DiceRolling.py
+++ DiceRolling.py
@@ -1,6 +1,7 @@
+#Library that allows for random numbers to be generated
 import random
 
-
+#function that simulates rolling a dice.  A random number between 1 and 6 is generated and returned.
 def rolldie():
     die = random.randint(1,6)
     return die
@@ -8,9 +9,11 @@ def rolldie():
 die1 = 1
 die2 = 1
 
+#Prompts the user for input
 print('Welcome to the dice rolling program')
 user_input = input('Enter 1 to roll one dice and 2 to roll two die and q to quit:')
 
+#loop that allows the user to continue to roll die until the exit condition is satisfied.
 while (user_input != 'q'):
     if (user_input == '1'):
         print('Your rolled a:', rolldie())
